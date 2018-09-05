32fc924e51c5a7f6f618997f10bd811f2fa13087
Loris Zinsou <the.lz.himself@gmail.com>
2018-09-06 01:39:27 +0200

    Add a reactors file as reminder

---
+++ aioevsourcing/reactors.py
@@ -0,0 +1,3 @@
+# base reactor, to trigger arbitrary command. Reactor are aware of context
+# Two important specialised reactors: projector (to update state in Daily),
+# and snapshot projector to optimize aggregate loading if needed.
