6a7bef6d4b22f431ae076e764785370f119cb818
Loris Zinsou <loris.zinsou@dailymotion.com>
2018-09-10 20:17:17 +0200

    Use asynctest to mock coroutines

--- tests/aioevsourcing/test_bus.py
+++ tests/aioevsourcing/test_bus.py
@@ -1,5 +1,5 @@
 import asyncio
-from unittest.mock import MagicMock
+from asynctest import CoroutineMock
 
 from abc import ABC
 from dataclasses import dataclass
@@ -7,11 +7,6 @@ from dataclasses import dataclass
 import pytest
 
 from aioevsourcing import aggregates, events
-
-
-class AsyncMock(MagicMock):
-    async def __call__(self, *args, **kwargs):
-        return super(AsyncMock, self).__call__(*args, **kwargs)
 
 
 @dataclass
@@ -26,15 +21,19 @@ dummy_bus_event_registry = events.EventRegistry(
     {DummyBusEvent.topic: DummyBusEvent}
 )
 
+
 @pytest.fixture
 def dummy_queue():
     queue = asyncio.Queue()
-    queue.put = AsyncMock()
+    queue.put = CoroutineMock()
     return queue
+
 
 @pytest.fixture
 def dummy_bus(dummy_queue):
-    return events.JsonEventBus(registry=dummy_bus_event_registry, queue=dummy_queue)
+    return events.JsonEventBus(
+        registry=dummy_bus_event_registry, queue=dummy_queue
+    )
 
 
 @pytest.mark.asyncio
