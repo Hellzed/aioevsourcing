a5a55936fc63765dd5aa9634e01ed477e7b8f48a
Loris Zinsou <loris.zinsou@dailymotion.com>
2018-09-19 16:02:48 +0200

    Don't use pylint on tests

--- tests/aioevsourcing/test_bus.py
+++ tests/aioevsourcing/test_bus.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 import asyncio
 from asynctest import MagicMock, CoroutineMock, create_autospec
 
--- tests/aioevsourcing/test_store.py
+++ tests/aioevsourcing/test_store.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 from dataclasses import dataclass
 
 import pytest
